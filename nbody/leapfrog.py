1feb6b5bffdc6efcc2a0bee95b86afef79298c7e
ZadrraS <ZadrraS@gmail.com>
2015-07-20 19:57:06 +0300

    Moved the gravitational constant out of leapfrog

--- nbody/leapfrog.py
+++ nbody/leapfrog.py
@@ -7,15 +7,13 @@ Created on Fri Mar 13 21:49:28 2015
 
 import numpy as np
 
-G = 6.67e-11
-
-def calc_vel_update(positions, mass, dt):
+def calc_vel_update(positions, mass, G, dt):
     r_ik3 = np.power(np.sum((positions[0] - positions[1])**2), 1.5)
     updates = G * mass * dt * (positions[1] - positions[0]) / r_ik3
 
     return updates
 
-def simulate_step(bodies, dt, dt_output=None):
+def simulate_step(bodies, dt, G=1.0, dt_output=None):
     current_t = 0
     current_step = 0
     N_bodies = bodies.r.shape[0]
@@ -26,7 +24,7 @@ def simulate_step(bodies, dt, dt_output=None):
         for k in range(N_bodies):
             if k == i:
                 continue
-            bodies.v[i, :] += 0.5 * calc_vel_update((bodies.r[i, :], bodies.r[k, :]), bodies.m[k], dt)
+            bodies.v[i, :] += 0.5 * calc_vel_update((bodies.r[i, :], bodies.r[k, :]), bodies.m[k], G, dt)
 
     while True:
         for i in range(N_bodies):
@@ -37,7 +35,7 @@ def simulate_step(bodies, dt, dt_output=None):
                 if k == i:
                     continue
 
-                bodies.v[i, :] += calc_vel_update((bodies.r[i, :], bodies.r[k, :]), bodies.m[k], dt)
+                bodies.v[i, :] += calc_vel_update((bodies.r[i, :], bodies.r[k, :]), bodies.m[k], G, dt)
 
         if current_step * dt_output <= current_t:
             current_step += 1
