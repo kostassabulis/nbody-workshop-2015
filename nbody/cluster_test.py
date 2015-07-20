37527c7da6150637dbfbb06788d73638b53812c9
ZadrraS <ZadrraS@gmail.com>
2015-07-20 23:41:53 +0300

    Added a file for constants

--- nbody/cluster_test.py
+++ nbody/cluster_test.py
@@ -7,17 +7,14 @@ import euler
 import leapfrog
 import icc
 import display
+import constants
 
-M_solar = 1.98e30
-AU = 1.5e11
-yr = 3.15569e7
-
-total_time = 10000 * yr
-dt = 10 * yr
-dt_output = 50 * yr
+total_time = 10000 * constants.YR
+dt = 10 * constants.YR
+dt_output = 50 * constants.YR
 
 bodies = icc.plummer(100, 1.0e14)
-bodies.m *= M_solar
+bodies.m *= constants.SOLAR_MASS
 
 body_history = np.zeros((total_time / dt_output + 1, bodies.r.shape[0], bodies.r.shape[1]))
 body_history[0, :, :] = bodies.r
@@ -25,7 +22,7 @@ body_history[0, :, :] = bodies.r
 
 snapshot_renderer = display.SnapshotRenderer(body_history, blocking=True, line_style="", marker_style=".", 
                                              history_length=0, fade=False, verbose=0)
-for i, current_t in enumerate(leapfrog.simulate_step(bodies, dt, G=6.67e-11, dt_output=dt_output)):
+for i, current_t in enumerate(leapfrog.simulate_step(bodies, dt, G=constants.G, dt_output=dt_output)):
     if current_t >= total_time:
         break
 
---
+++ nbody/constants.py
@@ -0,0 +1,6 @@
+"""Various astronomical constants"""
+
+SOLAR_MASS = 1.98e30
+AU = 1.5e11
+YR = 3.15569e7
+G = 6.67e-11
