cedf8bf2750cd932f30a0dccfb3f178ccad6f94a
Tomas Seniut <nikromancer0@gmail.com>
2015-07-21 10:45:41 +0300

    Fixed plotting scale

--- nbody/cluster_test.py
+++ nbody/cluster_test.py
@@ -13,7 +13,8 @@ total_time = 10000 * constants.YR
 dt = 10 * constants.YR
 dt_output = 50 * constants.YR
 
-bodies = icc.plummer(100, 1.0e14)
+d= 1.0e14
+bodies = icc.plummer(100, d)
 #bodies.m *= constants.SOLAR_MASS # tai jau daroma icc
 
 body_history = np.zeros((total_time / dt_output + 1, bodies.r.shape[0], bodies.r.shape[1]))
@@ -21,7 +22,7 @@ body_history[0, :, :] = bodies.r
 
 
 snapshot_renderer = display.SnapshotRenderer(body_history, blocking=True, line_style="", marker_style=".", 
-                                             history_length=0, fade=False, verbose=0)
+                                             history_length=0, fade=False, verbose=0, bounds=[-2*d, 2*d])
 for i, current_t in enumerate(leapfrog.simulate_step(bodies, dt, G=constants.G, dt_output=dt_output)):
     if current_t >= total_time:
         break
