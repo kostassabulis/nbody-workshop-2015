00db53026300bb53b7d98c7ac0b242892e073748
ZadrraS <ZadrraS@gmail.com>
2015-07-24 00:06:08 +0300

    SnapshotStorage now loads and saves files correctly

--- nbody/snapshots/util.py
+++ nbody/snapshots/util.py
@@ -8,9 +8,9 @@ def load_snapshots(directory_name, stack_coords=False):
     """Loads files by traversing a directory and reading in a filename sorted order"""
     data = []
     for root, dirs, files in os.walk(directory_name):
-        for file_name in sorted(files, key=lambda x: int(x.split(".")[-1])):
+        for file_name in sorted(files, key=lambda x: int(x.split(".")[-2])):
             #This needs fixing, but I'll leave it like this until we unify our formats
-            if "csv" in file_name.split("."):
+            if file_name.endswith("csv"):
                 bodies = np.loadtxt(os.path.join(root, file_name), delimiter=",", skiprows=1, unpack=stack_coords)
                 data.append(bodies)
 
@@ -22,3 +22,6 @@ def save_snapshot(snapshot, file_name):
     for i in range(snapshot.shape[0]):
         f.write("%e,%e,%e\n" % (snapshot[i, 0], snapshot[i, 1], snapshot[i, 2]))
     f.close()
+
+def construct_snapshot_name(directory, num):
+    return os.path.join(directory, "nbody_snapshot." + str(num) + ".csv")
