cd9caa6d8aa606d1b6599093e4afe806ab48602e
Tomas Seniut <nikromancer0@gmail.com>
2015-07-23 17:55:12 +0300

    Added smoothing parameter

--- nbody/cluster_test.py
+++ nbody/cluster_test.py
@@ -10,11 +10,12 @@ import constants
 from snapshots.display import SnapshotRenderer
 
 total_time = 10000 * constants.YR
-dt = 10 * constants.YR
-dt_output = 50 * constants.YR
+dt = 1 * constants.YR
+dt_output = 5 * constants.YR
 
+epsilon = 0.2 #Smoothing parameter
 d= 1.0e14
-bodies = icc.plummer(100, d)
+bodies = icc.uniform_distribution(500, d)
 #bodies.m *= constants.SOLAR_MASS # tai jau daroma icc
 
 body_history = np.zeros((total_time / dt_output + 1, bodies.r.shape[0], bodies.r.shape[1]))
@@ -22,8 +23,8 @@ body_history[0, :, :] = bodies.r
 
 
 snapshot_renderer = SnapshotRenderer(body_history, blocking=True, line_style="", marker_style=".", 
-                                     history_length=0, fade=False, verbose=0, bounds=[-2*d, 2*d])
-for i, current_t in enumerate(leapfrog.simulate_step(bodies, dt, G=constants.G, dt_output=dt_output)):
+                                     history_length=0, fade=False, verbose=0, bounds=[-1*d, 1*d])
+for i, current_t in enumerate(leapfrog.simulate_step(bodies, dt, constants.G, epsilon, dt_output=dt_output)):
     if current_t >= total_time:
         break
 
