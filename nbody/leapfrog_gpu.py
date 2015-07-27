39fc80f2b7ba1513a725590de6fc5bc95e28f120
Kostas Sabulis <kostas.sabulis@gmail.com>
2015-07-27 16:18:38 +0300

    Initial try on leapfrog with GPU

---
+++ nbody/leapfrog_gpu.py
@@ -0,0 +1,47 @@
+"""Leapfrog integrator"""
+
+import gnumpy as gpu
+import numpy as np
+
+def calculate_dt(V, A, N_bodies, alpha):
+    a_max = 0.
+    for i in range(N_bodies):
+        A = gpu.garray(A)
+        a = gpu.sum(A[i,:]**2)
+        if a > a_max:
+            a_max = a
+            a_max_index = i
+            
+    V = gpu.garray(V)
+    v = gpu.sqrt(gpu.sum(V[a_max_index,:]**2))
+    return alpha*v/a_max
+
+def simulate_step(bodies, dt_min, G, epsilon, dt_output, alpha):
+    current_t = 0
+    current_step = 0
+    N_bodies = bodies.r.shape[0]
+    A = np.zeros_like(bodies.v)
+    for i in range(N_bodies):
+        coord_diff = bodies.r - bodies.r[i, :]
+        r_ik3 = (gpu.sum(coord_diff**2, axis=1) + epsilon**2)**1.5 #+ 1e-16
+        A[i,:] = G*gpu.sum(bodies.m[:, np.newaxis] * coord_diff / r_ik3[:, np.newaxis], axis=0)
+        
+    dt = max(calculate_dt(bodies.v, A, N_bodies, alpha), dt_min)
+    bodies.v += 0.5 * dt * A
+
+    while True:
+        bodies.r += dt * bodies.v      
+        for i in range(N_bodies):        
+            coord_diff = bodies.r - bodies.r[i, :]
+            r_ik3 = (gpu.sum(coord_diff**2, axis=1) + epsilon**2)**1.5 #+ 1e-16
+            A[i,:] = G*gpu.sum(bodies.m[:, np.newaxis] * coord_diff / r_ik3[:, np.newaxis], axis=0)
+        
+        dt = max(calculate_dt(bodies.v, A, N_bodies, alpha), dt_min)
+        bodies.v += dt * A
+        if current_step * dt_output <= current_t:
+            current_step += 1
+            yield current_t
+            gpu.status()
+            
+        current_t += dt   
+        
