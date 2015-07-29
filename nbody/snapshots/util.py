7b697cbcddf29412ac94a186817bd9db1880a0f2
ZadrraS <zadrras@gmail.com>
2015-07-29 23:11:13 +0300

    Fix csv saving for arbitrary parameter sets

--- nbody/snapshots/util.py
+++ nbody/snapshots/util.py
@@ -11,17 +11,13 @@ def load_snapshots(directory_name, stack_coords=False):
         for file_name in sorted(files, key=lambda x: int(x.split(".")[-2])):
             #This needs fixing, but I'll leave it like this until we unify our formats
             if file_name.endswith("csv"):
-                bodies = np.loadtxt(os.path.join(root, file_name), delimiter=",", skiprows=1, unpack=stack_coords)
+                bodies = np.loadtxt(os.path.join(root, file_name), delimiter=",", unpack=stack_coords)
                 data.append(bodies)
 
     return np.array(data)
     
 def save_snapshot(snapshot, file_name):
-    f = open(file_name, 'w')
-    f.write("x,y,z\n")
-    for i in range(snapshot.shape[0]):
-        f.write("%e,%e,%e\n" % (snapshot[i, 0], snapshot[i, 1], snapshot[i, 2]))
-    f.close()
+    np.savetxt(file_name, snapshot, delimiter=",")
 
 def construct_snapshot_name(directory, num):
     return os.path.join(directory, "nbody_snapshot." + str(num) + ".csv")
