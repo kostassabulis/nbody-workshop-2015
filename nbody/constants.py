1cfbc230d6c63ccddeaeb1e9bbbf46a55aea9cd7
Rokas <Mythemind@users.noreply.github.com>
2015-08-03 17:06:00 +0300

    Code units calculation

--- nbody/constants.py
+++ nbody/constants.py
@@ -1,6 +1,13 @@
 """Various astronomical constants in SI units"""
 
-SOLAR_MASS = 1.98e30    #[kg]
-AU = 1.5e11             #[m]
-YR = 3.15569e7          #[s]
-G = 6.67e-11            #[m^3 kg^-1 s^-2]
+import scipy.constants as sc
+import numpy as np
+
+SOLAR_MASS = 1.98e30            #[kg]
+AU = sc.astronomical_unit       #1.5e11 [m]
+YR = sc.Julian_year             #~3.15569e7 [s]
+G = sc.G                        #6.67e-11  [m^3 kg^-1 s^-2]
+PC = sc.parsec                  #3.086e+16 [m]
+
+TU = np.sqrt(PC**3/SOLAR_MASS/G) #Time Unit [s]
+TUcoef = YR/TU
