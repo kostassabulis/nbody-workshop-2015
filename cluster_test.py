b88f804efc7b89790fddbd2870f6dfbada88d24f
Rokas <Mythemind@users.noreply.github.com>
2015-08-05 18:11:14 +0300

    Added transition to code units before calculation
    Jei norit skaičiuoti kodo vienetais, tai atkomentuokit 4 pakeistas eilutes (6, 11, 12 ir 32), tačiau snapshots turės kodo vienetus. Jei kas orientuojasi ką daro "SnapshotStorage()" (nes aš tai net neįsivaizduoju kur x kur y kur vx kur vy...žinau, kad tik masė paskutinis dydis) tai reik koordinates padauginti iš nb.constants.PC, greičius iš nb.constants.PC/nb.constants.TU, o masę iš nb.constants.SOLAR_MASS

--- cluster_test.py
+++ cluster_test.py
@@ -3,12 +3,13 @@ from nbody.snapshots.display import SnapshotRenderer
 from nbody.snapshots.storage import SnapshotStorage
 import numpy as np
 
+#nb.constants.G, nb.constants.SOLAR_MASS, nb.constants.YR = nb.constants.unitstocode() #conversion to code units [PC], [SOLAR_MASS] and G = 1
 total_time = 1000 * nb.constants.YR
 dt_min = 0.1 * nb.constants.YR
 dt_output = 1 * nb.constants.YR
 
-epsilon = nb.constants.AU #Smoothing parameter
-d = 1.0e14
+epsilon = nb.constants.AU#/nb.constants.PC #Smoothing parameter
+d = 1.0e14#/nb.constants.PC
 alpha = 0.0001 #adaptive time step parameter
 N = 500
 bodies, distribution = nb.icc.plummer(N, d)
@@ -28,7 +29,7 @@ for i, current_t in enumerate(nb.leapfrog_adaptive.simulate_step(bodies, dt_min,
 
     snapshot_storage.append(bodies)
 #   snapshot_renderer.display_step()
-
+#nb.constants.G, nb.constants.SOLAR_MASS, nb.constants.YR = nb.constants.codetounits() #Back to SI units
 name = '{:s}_N{:d}_T{:d}_E{:.0e}_d{:.0e}'.format(distribution, N, int(total_time / nb.constants.YR), epsilon, d)
 snapshot_renderer.run(name + "time_test.mp4")
 snapshot_storage.save("nbody/" + name + "time_test.pkl")
